38309caed4997292727c58797b6d0942c761167b
Clémence Clavel <clemence.clavel@hotmail.fr>
2023-02-25 13:24:08 -0500

    collision

--- Source/WonderJam2023/Private/Characters/WJCultCharacter.cpp
+++ Source/WonderJam2023/Private/Characters/WJCultCharacter.cpp
@@ -5,12 +5,20 @@
 #include "Characters/Controller/WJCultController.h"
 #include "Navigation/PathFollowingComponent.h"
 #include "AITypes.h"
+#include "Components/SphereComponent.h"
 
 // Sets default values
 AWJCultCharacter::AWJCultCharacter()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	SphereCollider = CreateDefaultSubobject<USphereComponent>(TEXT("Attacking Cullusion"));
+	SphereCollider->SetupAttachment(RootComponent);
+	SphereCollider->SetSphereRadius(700.f);
+	SphereCollider->SetGenerateOverlapEvents(true);
+	SphereCollider->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	SphereCollider->bHiddenInGame = false;
+
 }
 
 void AWJCultCharacter::OnAIMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult& Result)
@@ -22,13 +30,19 @@ void AWJCultCharacter::Convert()
 {
 }
 
+void AWJCultCharacter::OnCharacterDetected(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("We got a collision."));
+}
+
 // Called when the game starts or when spawned
 void AWJCultCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	CultController = Cast<AWJCultController>(GetController());
-	CultController->GetPathFollowingComponent()->OnRequestFinished.AddUObject
-	(this, &AWJCultCharacter::OnAIMoveCompleted);
+	CultController->GetPathFollowingComponent()->OnRequestFinished.AddUObject(this, &AWJCultCharacter::OnAIMoveCompleted);
+	SphereCollider->OnComponentBeginOverlap.AddDynamic(this, &AWJCultCharacter::OnCharacterDetected);
 }
 
 
--- Source/WonderJam2023/Public/Characters/WJCultCharacter.h
+++ Source/WonderJam2023/Public/Characters/WJCultCharacter.h
@@ -21,6 +21,15 @@ public:
 
 	void Convert();
 
+	//Is the character converted to the cult
+	bool bIsConverted = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sphere Collider")
+	class USphereComponent* SphereCollider;
+	
+	UFUNCTION()
+	void OnCharacterDetected(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
