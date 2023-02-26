4983c436850c84edd96e7c3130087553b450dcc0
Zaraky-Kenpachi <kenpachi.zaken@gmail.com>
2023-02-26 12:18:23 -0500

    Adding assistant to get point of interest of desired type.

---
+++ Source/WonderJam2023/Private/Gameplay/WJPointOfInterestAssistant.cpp
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Gameplay/WJPointOfInterestAssistant.h"
+
+
+// Sets default values
+AWJPointOfInterestAssistant::AWJPointOfInterestAssistant()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+FVector3d AWJPointOfInterestAssistant::GetPointOfInterest(PointOfInterestType typeOfPoI)
+{
+	
+	switch (typeOfPoI)
+	{
+	case Hospital:
+		return GetHospitalLocationVector();
+	case Restaurant:
+		return GetRestaurantLocationVector();
+	case CultBase:
+		return GetCultBaseLocationVector();
+	default:
+		return FVector3d().Zero();
+	}
+}
+
+// Called when the game starts or when spawned
+void AWJPointOfInterestAssistant::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+FVector3d AWJPointOfInterestAssistant::GetHospitalLocationVector()
+{
+	int chance = FMath::RandRange(0, Hospitals.Num()-1);
+	return Hospitals[chance]->GetTransform().GetLocation();
+}
+
+FVector3d AWJPointOfInterestAssistant::GetRestaurantLocationVector()
+{
+	int chance = FMath::RandRange(0, Restaurants.Num()-1);
+	return Restaurants[chance]->GetTransform().GetLocation();
+}
+
+FVector3d AWJPointOfInterestAssistant::GetCultBaseLocationVector()
+{
+	int chance = FMath::RandRange(0, CultBases.Num()-1);
+	return CultBases[chance]->GetTransform().GetLocation();
+}
---
+++ Source/WonderJam2023/Public/Gameplay/WJPointOfInterestAssistant.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WJPointOfInterestAssistant.generated.h"
+
+UENUM()
+enum PointOfInterestType
+{
+	Hospital,
+	Restaurant,
+	CultBase
+};
+
+UCLASS()
+class WONDERJAM2023_API AWJPointOfInterestAssistant : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWJPointOfInterestAssistant();
+
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Point of Interest")
+	TArray<AActor*> Hospitals;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Point of Interest")
+	TArray<AActor*> Restaurants;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Point of Interest")
+	TArray<AActor*> CultBases;
+
+	UFUNCTION()
+	FVector3d GetPointOfInterest(PointOfInterestType typeOfPoI);
+
+
+protected:
+	virtual void BeginPlay() override;
+	FVector3d GetHospitalLocationVector();
+	FVector3d GetRestaurantLocationVector();
+	FVector3d GetCultBaseLocationVector();
+	
+};
